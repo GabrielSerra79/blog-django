dff39a4698b30d2fc7e2718ec805ffb6cf88a3a1
GabrielSerra79 <bielserra@gmail.com>
2026-02-03 18:35:49 -0300

    Aula 548

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -6,7 +6,37 @@ PER_PAGE = 9
 
 
 def index(request):
-    posts = Post.objects.get_published() # type: ignore
+    posts = Post.objects.get_published()# type: ignore
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def created_by(request, author_pk):
+    posts = Post.objects.get_published().filter(created_by__pk=author_pk)  # type: ignore
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(category__slug=slug)  # type: ignore
 
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
@@ -21,7 +51,7 @@ def index(request):
     )
 
 
-def page(request):
+def page(request, slug):
     return render(
         request,
         'blog/pages/page.html',
@@ -31,11 +61,13 @@ def page(request):
     )
 
 
-def post(request):
+def post(request, slug):
+    post = Post.objects.get_published().filter(slug=slug).first() # type: ignore
+
     return render(
         request,
         'blog/pages/post.html',
         {
-            # 'page_obj': page_obj,
+            'post': post,
         }
     )
