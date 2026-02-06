7756c5d5a518c596ac1ceed8a5798d61c2b852a9
GabrielSerra79 <bielserra@gmail.com>
2026-02-06 15:40:30 -0300

    Aula 557

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,27 +1,43 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from blog.models import Post, Page, Tag
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import Http404
+from django.views.generic.list import ListView
 
 PER_PAGE = 9
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/pages/index.html'
+    context_object_name = 'posts'
+    ordering = '-pk',
+    paginate_by = PER_PAGE
+    queryset = Post.objects.get_published() # type: ignore
 
-def index(request):
-    posts = Post.objects.get_published()  # type: ignore
+    # def get_queryset(self) -> QuerySet[Any]:
+    #     queryset = super().get_queryset()
+    #     queryset = queryset.filter(is_published=True)
+    #     return queryset
 
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
 
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
-        }
-    )
+# def index(request):
+#     posts = Post.objects.get_published()  # type: ignore
+
+#     paginator = Paginator(posts, PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
+
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': page_obj,
+#         }
+#     )
 
 
 def created_by(request, author_pk):
